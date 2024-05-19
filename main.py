d7a73761f31e7088828a0182b8e3b62b8c576f70
mnilsen <32636970+ostepizza@users.noreply.github.com>
2024-05-19 14:29:03 +0200

    Tire stretch calculation works

--- main.py
+++ main.py
@@ -1,71 +1,92 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
-# rims
-rim_diameter_inch = 17
-rim_width_inch = 10
+########################################
 
-# tires
-tire_width_mm = 205
-tire_height_relative = 45
-tire_diameter_inch = 17
-
-# image 
+# IMAGE VARS
 image_width = 500
 image_height = 500
 image_dpi = 16
+outline_width = 2
+outline_rim_color = 'white'
+outline_tire_color = 'red'
+font_color = 'red'
+
+# INPUT VARS
+rim_diameter_inch = 17
+rim_width_inch = 7.5
+tire_width_mm = 205
+tire_height_relative = 45
+
+########################################
+
+# Extra inches to simulate rim diameter and lip
+rim_diameter_additional = 0
+rim_width_additional = 1
+
+# Convert rim size in inches to pixels
+rim_diameter = (rim_diameter_inch + rim_diameter_additional) * image_dpi
+rim_width = (rim_width_inch + rim_width_additional) * image_dpi
+
+# Convert tire size in inches to pixels
+tire_width_inch = tire_width_mm / 25.4
+tire_width_t = tire_width_inch * image_dpi # top tire width is th
+tire_width_b = rim_width # bottom tire width is the same as rim width due to seating
+
+# Calculate difference between top and bottom tire width, to get a relative height using sidewall length
+tire_area = tire_width_t * (tire_width_t * (tire_height_relative / 100))
+average_width = (tire_width_t + tire_width_b) / 2
+tire_height = tire_area / average_width
+
+print("Rim diameter: ", rim_diameter)
+print("Rim width: ", rim_width)
+print("Tire width: ", tire_width_t, tire_width_b)
+print("Tire height: ", tire_height)
+
+# Coordinates of the rim (since rim is square, only four are needed)
+rsx = image_width / 2 - rim_width / 2
+rsy = image_width / 2 - rim_diameter / 2
+rex = rsx + rim_width
+rey = rsy + rim_diameter
+
+# Coordinates of the "top" tyre (this can probably be cut down, but is somewhat easier to read)
+# tsxb = tire start x bottom, teyt = tire end y top
+tsxb = rsx
+tsyb = rsy
+texb = tsxb + tire_width_b
+teyb = rsy
+tsxt = rsx - (tire_width_t - rim_width) / 2
+tsyt = rsy - tire_height
+text = tsxt + tire_width_t
+teyt = tsyt
+
+# Coordinates of the "bottom" tyre
+tsxb2 = rsx
+tsyb2 = rey
+texb2 = tsxb2 + tire_width_b
+teyb2 = rey
+tsxt2 = rsx - (tire_width_t - rim_width) / 2
+tsyt2 = rey + tire_height
+text2 = tsxt2 + tire_width_t
+teyt2 = tsyt2
 
 
-# prepare the rims:
-rim_diameter_pixel = rim_diameter_inch * image_dpi
-rim_width_pixel = rim_width_inch * image_dpi
-
-# prepare the tires:
-tire_width_inch = tire_width_mm * 0.0393701
-tire_width_pixel = tire_width_inch * image_dpi
-tire_height_pixel = tire_width_pixel * (tire_height_relative / 100)
-tire_diameter_pixel = tire_diameter_inch * image_dpi
-
-print("Rim diameter in pixels: ", rim_diameter_pixel)
-print("Rim width in pixels: ", rim_width_pixel)
-print("Tire width in pixels: ", tire_width_pixel)
-print("Tire height in pixels: ", tire_height_pixel)
-print("Tire diameter in pixels: ", tire_diameter_pixel)
-
-
-# coordinates of the rim
-rsx = image_width / 2 - rim_width_pixel / 2
-rsy = image_width / 2 - rim_diameter_pixel / 2
-rex = rsx + rim_width_pixel
-rey = rsy + rim_diameter_pixel
-
-# coordinates for tire, top
-tsxt = rsx + rim_width_pixel / 2 - tire_width_pixel / 2
-tsyt = rsy - tire_height_pixel
-text = tsxt + tire_width_pixel
-teyt = tsyt + tire_height_pixel
-
-# coordinates for tire, bottom
-tsxb = rsx + rim_width_pixel / 2 - tire_width_pixel / 2
-tsyb = rsy + rim_diameter_pixel
-texb = tsxb + tire_width_pixel
-teyb = tsyb + tire_height_pixel
-
-
-# Create a new image with white background
-img = Image.new('RGB', (500, 500), 'black')
-
-# Create a draw object
+# Set up image and draw object
+img = Image.new('RGB', (image_width, image_height), 'black')
 draw = ImageDraw.Draw(img)
 
-# Define the points for the square
+# Define points for rim, tire top and tire bottom, and draw them
 rim_coordinates = [(rsx, rsy), (rex, rsy), (rex, rey), (rsx, rey)]
-tire_coordinates_top = [(tsxt, tsyt), (text, tsyt), (text, teyt), (tsxt, teyt)]
-tire_coordinates_bottom = [(tsxb, tsyb), (texb, tsyb), (texb, teyb), (tsxb, teyb)]
+tire_coordinates_top = [(tsxt, tsyt), (text, teyt), (texb, teyb), (tsxb, tsyb)]
+tire_coordinates_bottom = [(tsxt2, tsyt2), (text2, teyt2), (texb2, teyb2), (tsxb2, tsyb2)]
+draw.polygon(rim_coordinates, outline=outline_rim_color, width=outline_width)
+draw.polygon(tire_coordinates_top, outline=outline_tire_color, width=outline_width)
+draw.polygon(tire_coordinates_bottom, outline=outline_tire_color, width=outline_width)
 
-# Draw the square
-draw.polygon(rim_coordinates, outline='white')
-draw.polygon(tire_coordinates_top, outline='red')
-draw.polygon(tire_coordinates_bottom, outline='blue')
+# Text to display rim and tire size
+font = ImageFont.truetype("arial.ttf", 15)
+text_position = (10, 10)
+text = str(rim_diameter_inch) + "x" + str(rim_width_inch) + ", " + str(tire_width_mm) + "/" + str(tire_height_relative) + "R" + (str(rim_diameter_inch))
+draw.text(text_position, text, fill=font_color, font=font)
 
 # Save the image
-img.save('square.png')
+img.save('result.png')
